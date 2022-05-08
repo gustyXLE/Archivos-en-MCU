e1ac0c6b6872039102b7bc08444c7e53fe03c318
Gustavo Garcia <gustyXLE>
2022-05-08 20:10:30 -0300

    Cambio de prueba

---
+++ test_code.c
@@ -0,0 +1,16 @@
+/*
+ * test_code.c
+ *
+ *  Created on: 8 may. 2022
+ *      Author: gusty
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+
+int main(){
+	printf("Probando GitHub!\n");
+	getch();
+	return 0;
+}
+
